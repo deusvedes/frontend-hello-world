6aad1e3ac488887fe5cf2181a783da3e4304ca25
Nico Weinberg <nico.weinberg@gmail.com>
2023-04-03 11:47:29 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {World} from "./shared/world.model";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  constructor(private http: HttpClient) { }
+
   title = 'frontend-hello-world';
+  host = 'https://taaweni0-hello-world-hello-world.azuremicroservices.io';
+  rootUrl = '/hello';
+  public world: World;
+
+  getHello() {
+    console.log('GET Hello called');
+    return this.http.get<World>(this.rootUrl).subscribe((world: World) => {
+      console.log("world:" + world)
+      this.world = world;
+    });
+  }
 }
---
+++ src/app/shared/world.model.ts
@@ -0,0 +1,5 @@
+export class World {
+  timestamp: Date;
+  uuid: string;
+  hello: string;
+}
